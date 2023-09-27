ecbfafb52b85a3f718df13c9232902949aa396b2
majidalykhan <majidalykhan@gmail.com>
2023-09-27 19:05:52 +0500

    Updating with useReducer

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,14 +1,23 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 
 const BlogContext = React.createContext();
 
-const [blogPosts, setBlogPosts] = useState([]);
-
-const addBlogPost = () => {
-  setBlogPosts([...blogPosts, { title: `Blog Post #${blogPosts.length + 1}` }]);
+const blogReducer = (state, action) => {
+  switch (action.type) {
+    case "add_blogPost":
+      return [...state, { title: `Blog Post #${state.length + 1}` }];
+    default:
+      return state;
+  }
 };
 
 export const BlogProvider = ({ children }) => {
+  const [blogPosts, dispatch] = useReducer(blogReducer, []);
+
+  const addBlogPost = () => {
+    dispatch({ type: "add_blogPost" });
+  };
+
   return (
     <BlogContext.Provider value={{ data: blogPosts, addBlogPost }}>
       {children}
--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,14 +1,15 @@
 import React, { useContext } from "react";
-import { FlatList, StyleSheet, Text, View } from "react-native";
+import { FlatList, StyleSheet, Text, View, Button } from "react-native";
 import BlogContext from "../context/BlogContext";
 
 const IndexScreen = () => {
-  const BlogPosts = useContext(BlogContext);
+  const { data, addBlogPost } = useContext(BlogContext);
   return (
     <View>
       <Text>Index Screen</Text>
+      <Button title="Add Post" onPress={addBlogPost} />
       <FlatList
-        data={BlogPosts}
+        data={data}
         keyExtractor={(blogPost) => blogPost.title}
         renderItem={({ item }) => {
           return <Text>{item.title}</Text>;
