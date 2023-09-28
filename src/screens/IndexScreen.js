47d9becb76a9c1a21cac68acb9dfac28eca245d9
majidalykhan <majidalykhan@gmail.com>
2023-09-28 17:09:46 +0500

    Styling

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,24 +1,64 @@
 import React, { useContext } from "react";
 import { FlatList, StyleSheet, Text, View, Button } from "react-native";
+import { ScrollView } from "react-native-virtualized-view";
 import { Context } from "../context/BlogContext";
+import { Feather } from "@expo/vector-icons";
 
 const IndexScreen = () => {
   const { state, addBlogPost } = useContext(Context);
   return (
     <View>
-      <Text>Index Screen</Text>
-      <Button title="Add Post" onPress={addBlogPost} />
-      <FlatList
-        data={state}
-        keyExtractor={(blogPost) => blogPost.title}
-        renderItem={({ item }) => {
-          return <Text>{item.title}</Text>;
-        }}
-      />
+      <View style={styles.listView}>
+        <ScrollView>
+          <FlatList
+            data={state}
+            keyExtractor={(blogPost) => blogPost.title}
+            renderItem={({ item }) => {
+              return (
+                <View style={styles.row}>
+                  <Text>{item.title}</Text>
+                  <Feather style={styles.icon} name="trash" />
+                </View>
+              );
+            }}
+          />
+        </ScrollView>
+      </View>
+      <View style={styles.buttonView}>
+        <Button color="#000000" title="Add Post" onPress={addBlogPost} />
+      </View>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  listView: {
+    marginTop: 20,
+    height: 500,
+    borderWidth: 1,
+    borderColor: "grey",
+  },
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingVertical: 20,
+    paddingHorizontal: 10,
+    borderBottomWidth: 1,
+    borderColor: "gray",
+  },
+  title: {
+    fontSize: 24,
+  },
+  icon: {
+    fontSize: 24,
+  },
+  buttonView: {
+    marginTop: 20,
+    marginLeft: 50,
+    marginRight: 50,
+    borderRadius: 20,
+    overflow: "hidden",
+  },
+});
 
 export default IndexScreen;
