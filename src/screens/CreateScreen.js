edbea151bb6d57448a43b8e1f954dc0c64c5f605
majidalykhan <majidalykhan@gmail.com>
2023-10-24 00:49:13 +0500

    Displaying a form

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -1,14 +1,51 @@
-import { StyleSheet, Text, View } from "react-native";
-import React, { useContext } from "react";
+import { StyleSheet, Text, TextInput, View, Button } from "react-native";
+import React, { useContext, useState } from "react";
 
 const CreateScreen = () => {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
   return (
     <View>
-      <Text>Create Screen</Text>
+      <Text style={styles.label}>Enter Title</Text>
+      <TextInput
+        style={styles.input}
+        value={title}
+        onChangeText={(text) => setTitle(text)}
+      />
+      <Text style={styles.label}>Enter Description</Text>
+      <TextInput
+        style={styles.input}
+        value={description}
+        onChangeText={(text) => setTitle(text)}
+      />
+      <View style={styles.buttonView}>
+        <Button color="#000000" title="Add Post" />
+      </View>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  label: {
+    fontSize: 20,
+    marginBottom: 10,
+    padding: 5,
+  },
+  input: {
+    fontSize: 18,
+    borderWidth: 1,
+    borderColor: "grey",
+    marginLeft: 5,
+    marginRight: 5,
+  },
+  buttonView: {
+    marginTop: 20,
+    marginLeft: 50,
+    marginRight: 50,
+    borderRadius: 20,
+    overflow: "hidden",
+  },
+});
 
 export default CreateScreen;
